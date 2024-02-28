4dcc5732bc821dfd342297a393ea2bc208c15352
Adam Osusky <adam.osusky02@gmail.com>
2024-02-28 16:53:07 +0100

    Doc: representation of document - one document is compoesed of document pages

---
+++ src/main/java/cz/cuni/mff/idp/doc/Doc.java
@@ -0,0 +1,10 @@
+package src.main.java.cz.cuni.mff.idp.doc;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Doc {
+    public List<DocPage> pages = new LinkedList<>();
+
+    public static class DocPage{}
+}
