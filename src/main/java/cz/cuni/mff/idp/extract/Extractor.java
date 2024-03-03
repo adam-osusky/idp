b735d7a880b3d935849d96289d7686f3aa3bd9d9
Adam Osusky <adam.osusky02@gmail.com>
2024-03-03 14:59:29 +0100

    extract: extraction from ocr result - simply iterate through all words and check if their bbx intersection with target bbox is atleeat 0.5 of th words bbox

--- src/main/java/cz/cuni/mff/idp/extract/Extractor.java
+++ src/main/java/cz/cuni/mff/idp/extract/Extractor.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 public class Extractor {
     Map<Integer, List<Target>> targets = new HashMap<>();
+    float intersectionThreshold = 0.5F;
 
     public Extractor(String configFile) {
         JsonObject config = loadJson(configFile);
@@ -46,7 +47,7 @@ public class Extractor {
             JsonObject targetJson = targetArray.get(i).getAsJsonObject();
 
             try {
-                String name = targetJson.get("variable_name").toString();
+                String name = targetJson.get("variable_name").getAsString();
                 int page = targetJson.get("page").getAsInt();
                 int left = targetJson.get("left").getAsInt();
                 int top = targetJson.get("top").getAsInt();
@@ -61,9 +62,52 @@ public class Extractor {
         }
     }
 
-    public JsonObject extract(OcrResult ocrResult) {
+    public JsonArray extractPage(OcrResult ocrResult, int page) {
+        var pageTargets = targets.get(page);
+        JsonArray pageExtractions = new JsonArray();
 
-        return null;
+        for (var word : ocrResult.words) {
+            float area = word.bbox().area();
+
+            for (var target : pageTargets) {
+                float intersection = word.bbox().intersection(target.bbox()) / area;
+
+                if (intersection >= intersectionThreshold) {
+                    pageExtractions.add(createExtractObject(target.variableName(), page, word));
+                }
+
+            }
+        }
+
+        return pageExtractions;
+    }
+
+    private static JsonObject createExtractObject(String variableName, int page, OcrResult.Word word) {
+        JsonObject targetObject = new JsonObject();
+        targetObject.addProperty("variable_name", variableName);
+        targetObject.addProperty("page", page);
+
+        targetObject.addProperty("extracted", word.str());
+
+        targetObject.addProperty("left", word.bbox().left());
+        targetObject.addProperty("top", word.bbox().top());
+        targetObject.addProperty("right", word.bbox().right());
+        targetObject.addProperty("bottom", word.bbox().bottom());
+
+        return targetObject;
+    }
+
+    public JsonObject extract(List<OcrResult> ocrResultList) {
+        JsonObject extractions = new JsonObject();
+
+        int page = 1;
+        for (var ocrResult : ocrResultList) {
+            JsonArray pageExtractions = extractPage(ocrResult, page);
+            extractions.add(String.valueOf(page), pageExtractions);
+            page++;
+        }
+
+        return extractions;
     }
 
 }
